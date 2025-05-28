9413344cf98c02eebb1b2484cc88cd3574173125
Ivan Santos <ivancsantos@hotmail.com.br>
2025-05-28 17:52:27 -0300

    Adiciona modelo DadosEpisodio

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
@@ -17,18 +18,24 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://omdbapi.com/?t=gilmore+girls&apikey=ffa3e7c8");
+		ConverteDados conversor = new ConverteDados();
 
-
-		System.out.println(json);
-
+		// Um teste de consumo de API que retorna uma foto de café aleatória
 		//json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
 		//System.out.println(json);
 
-		ConverteDados conversor = new ConverteDados();
+		// Dados sobre série
+		var json = consumoAPI.obterDados("https://omdbapi.com/?t=gilmore+girls&apikey=ffa3e7c8");
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(json);
 		System.out.println(dados);
 
+
+		// Dados sobre episório
+		json = consumoAPI.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=ffa3e7c8");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(json);
+		System.out.println(dadosEpisodio);
 	}
 
 
